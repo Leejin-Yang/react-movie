71bc1cbd2c5390d70e4cec3dcbad50ac56ed71a4
Leejin-Yang <ejin1996@gmail.com>
2022-05-10 16:00:37 +0900

    feat: 상단 검색 폼 구현

--- src/routes/SearchPage/index.tsx
+++ src/routes/SearchPage/index.tsx
@@ -1,9 +1,15 @@
+import { SearchIcon } from 'assets/svgs';
 import styles from './SearchPage.module.scss';
 
 const SearchPage = () => {
   return (
     <main className={styles.searchPage}>
-      <h1>Search Page</h1>
+      <form className={styles.searchForm}>
+        <input type='text' placeholder='제목 검색' />
+        <button type='submit'>
+          <SearchIcon className={styles.icon} />
+        </button>
+      </form>
     </main>
   );
 };
