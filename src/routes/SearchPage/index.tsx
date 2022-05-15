909239e55f760b5b3cfc32eba9fa598cc5b3bf27
Leejin-Yang <ejin1996@gmail.com>
2022-05-15 12:30:13 +0900

    fix: 리퀘스트 에러 처리 수정

--- src/routes/SearchPage/index.tsx
+++ src/routes/SearchPage/index.tsx
@@ -14,6 +14,7 @@ import { movieListState, pageNumberState } from 'states/movie';
 const NO_RESULT = '검색 결과가 없습니다.';
 const NO_SEARCH_WORD = '검색어를 입력해주세요.';
 const TOO_MANY_RESULT = '검색 결과가 많습니다.';
+const NET_ERROR = '현재 검색이 불가능합니다.';
 
 const SearchPage = () => {
   const [isLoading, setIsLoding] = useState<boolean>(false);
@@ -47,6 +48,9 @@ const SearchPage = () => {
         }
 
         setMovieList((prev) => [...prev, ...data.Search]);
+      })
+      .catch(() => {
+        setErrorMessage(NET_ERROR);
       });
     setIsLoding(false);
   }, [currentSearch, pageNumber, ref, setMovieList]);
