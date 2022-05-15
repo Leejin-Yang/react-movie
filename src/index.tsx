24bf43265b86d6371ff3b2844763a5c1cbc67cc8
Leejin-Yang <ejin1996@gmail.com>
2022-05-15 12:51:07 +0900

    chore: gh-pages 설정

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes />
       </BrowserRouter>
     </RecoilRoot>
--- src/services/movie.ts
+++ src/services/movie.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { IMovieAPIRes } from 'types/movie.d';
 
-const BASE_URL = 'http://www.omdbapi.com/';
+const BASE_URL = 'https://www.omdbapi.com/';
 
 interface Params {
   s: string;
